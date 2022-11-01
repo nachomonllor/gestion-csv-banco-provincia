6fec3c8805968bab72a4a774a22f98e962961f3c
nachomonllor <nachomonllorc@gmail.com>
2022-11-01 16:57:38 -0300

    Lector csv banco provincia

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbl_importe_buscado = new System.Windows.Forms.Label();
+            this.btn_sumatoria = new System.Windows.Forms.Button();
+            this.txt_contiene = new System.Windows.Forms.TextBox();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Importe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Saldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_carga_datagrid = new System.Windows.Forms.Button();
+            this.btn_reset_datagrid = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Fecha,
+            this.Descripcion,
+            this.Importe,
+            this.Saldo});
+            this.dataGridView1.Location = new System.Drawing.Point(37, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(987, 400);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lbl_importe_buscado
+            // 
+            this.lbl_importe_buscado.AutoSize = true;
+            this.lbl_importe_buscado.Location = new System.Drawing.Point(361, 562);
+            this.lbl_importe_buscado.Name = "lbl_importe_buscado";
+            this.lbl_importe_buscado.Size = new System.Drawing.Size(133, 13);
+            this.lbl_importe_buscado.TabIndex = 1;
+            this.lbl_importe_buscado.Text = "sumatoria importe buscado";
+            // 
+            // btn_sumatoria
+            // 
+            this.btn_sumatoria.Location = new System.Drawing.Point(364, 519);
+            this.btn_sumatoria.Name = "btn_sumatoria";
+            this.btn_sumatoria.Size = new System.Drawing.Size(130, 23);
+            this.btn_sumatoria.TabIndex = 2;
+            this.btn_sumatoria.Text = "Buscar Sumatoria";
+            this.btn_sumatoria.UseVisualStyleBackColor = true;
+            this.btn_sumatoria.Click += new System.EventHandler(this.btn_sumatoria_Click);
+            // 
+            // txt_contiene
+            // 
+            this.txt_contiene.Location = new System.Drawing.Point(364, 480);
+            this.txt_contiene.Name = "txt_contiene";
+            this.txt_contiene.Size = new System.Drawing.Size(130, 20);
+            this.txt_contiene.TabIndex = 3;
+            this.txt_contiene.Text = "Buscar importe";
+            // 
+            // Fecha
+            // 
+            this.Fecha.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Fecha.FillWeight = 162.4366F;
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            this.Fecha.ReadOnly = true;
+            this.Fecha.Width = 62;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Descripcion.FillWeight = 192.2112F;
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            // 
+            // Importe
+            // 
+            this.Importe.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Importe.FillWeight = 19.54571F;
+            this.Importe.HeaderText = "Importe";
+            this.Importe.Name = "Importe";
+            this.Importe.ReadOnly = true;
+            // 
+            // Saldo
+            // 
+            this.Saldo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Saldo.FillWeight = 25.80657F;
+            this.Saldo.HeaderText = "Saldo";
+            this.Saldo.Name = "Saldo";
+            this.Saldo.ReadOnly = true;
+            // 
+            // btn_carga_datagrid
+            // 
+            this.btn_carga_datagrid.Location = new System.Drawing.Point(527, 519);
+            this.btn_carga_datagrid.Name = "btn_carga_datagrid";
+            this.btn_carga_datagrid.Size = new System.Drawing.Size(232, 23);
+            this.btn_carga_datagrid.TabIndex = 4;
+            this.btn_carga_datagrid.Text = "Cargar datagrid Buscar";
+            this.btn_carga_datagrid.UseVisualStyleBackColor = true;
+            this.btn_carga_datagrid.Click += new System.EventHandler(this.btn_carga_datagrid_Click);
+            // 
+            // btn_reset_datagrid
+            // 
+            this.btn_reset_datagrid.Location = new System.Drawing.Point(37, 477);
+            this.btn_reset_datagrid.Name = "btn_reset_datagrid";
+            this.btn_reset_datagrid.Size = new System.Drawing.Size(171, 23);
+            this.btn_reset_datagrid.TabIndex = 5;
+            this.btn_reset_datagrid.Text = "Reset DataGrid";
+            this.btn_reset_datagrid.UseVisualStyleBackColor = true;
+            this.btn_reset_datagrid.Click += new System.EventHandler(this.btn_reset_datagrid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 609);
+            this.Controls.Add(this.btn_reset_datagrid);
+            this.Controls.Add(this.btn_carga_datagrid);
+            this.Controls.Add(this.txt_contiene);
+            this.Controls.Add(this.btn_sumatoria);
+            this.Controls.Add(this.lbl_importe_buscado);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbl_importe_buscado;
+        private System.Windows.Forms.Button btn_sumatoria;
+        private System.Windows.Forms.TextBox txt_contiene;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Importe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saldo;
+        private System.Windows.Forms.Button btn_carga_datagrid;
+        private System.Windows.Forms.Button btn_reset_datagrid;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string ruta = "C:\\Users\\nacho\\OneDrive\\Desktop\\Banco provincia csv meses\\unidos.csv";
+        static List<List<string>> datos = new List<List<string>>();
+        static List<Tuple<string, string, double, double>> listaDatos = new List<Tuple<string, string, double, double>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            datos = ObtenerDatos(ruta);
+            CargarListaDatos();
+            CargarDataGridView();
+        }
+
+        public void ResetDataGrid()
+        {
+            dataGridView1.Rows.Clear();
+            listaDatos = new List<Tuple<string, string, double, double>>();
+            CargarListaDatos();
+            CargarDataGridView();
+        }
+
+        private static string[] SmartSplit(string line, char separator = ',')
+        {
+            var inQuotes = false;
+            var token = "";
+            var lines = new List<string>();
+            for (var i = 0; i < line.Length; i++)
+            {
+                var ch = line[i];
+                if (inQuotes) // process string in quotes, 
+                {
+                    if (ch == '"')
+                    {
+                        if (i<line.Length-1 && line[i + 1] == '"')
+                        {
+                            i++;
+                            token += '"';
+                        }
+                        else inQuotes = false;
+                    }
+                    else token += ch;
+                }
+                else
+                {
+                    if (ch == '"') inQuotes = true;
+                    else if (ch == separator)
+                    {
+                        lines.Add(token);
+                        token = "";
+                    }
+                    else token += ch;
+                }
+            }
+            lines.Add(token);
+            return lines.ToArray();
+        }
+
+        public static List<List<string>> ObtenerDatos(string ruta)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(ruta);
+
+            List<List<string>> datos = new List<List<string>>();
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string[] fila = SmartSplit(lines[i]);
+                datos.Add(fila.ToList());
+
+            }
+
+            return datos;
+        }
+
+        public static bool IsNumeric( string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c) && c != '.' && c != ',')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        void CargarListaDatos()
+        {
+            foreach (List<string> fila in datos)
+            {
+                string fecha = fila[0];
+                string descripcion = fila[1];
+                string importe = fila[2].TrimStart('-');
+                string saldo = fila[3].TrimStart('-');
+
+                double importe_double = 0.0;
+                double saldo_double = 0.0;
+
+                if (importe.Length > 0  && IsNumeric(importe))
+                {
+                    importe_double = double.Parse(importe);
+                }
+                if (saldo.Length > 0 && IsNumeric(saldo))
+                {
+                    saldo_double = double.Parse(saldo);
+                }
+
+                if (fecha.Length > 0 && descripcion.Length > 0 && importe_double > 0)
+                {
+                    listaDatos.Add(new Tuple<string, string, double, double>(fecha, descripcion, importe_double, saldo_double));
+                }
+            }
+        }
+
+        void CargarListaDatos(string contiene)
+        {
+            foreach (List<string> fila in datos)
+            {
+                string fecha = fila[0];
+                string descripcion = fila[1];
+                string importe = fila[2].TrimStart('-');
+                string saldo = fila[3].TrimStart('-');
+
+                double importe_double = 0.0;
+                double saldo_double = 0.0;
+
+                if (importe.Length > 0  && IsNumeric(importe))
+                {
+                    importe_double = double.Parse(importe);
+                }
+                if (saldo.Length > 0 && IsNumeric(saldo))
+                {
+                    saldo_double = double.Parse(saldo);
+                }
+
+                //dataGridView1.Rows.Add(fecha, descripcion, importe_double, saldo_double);
+                if (fecha.Length > 0 && descripcion.Length > 0 && importe_double > 0)
+                {
+                    if (descripcion.Contains(contiene))
+                    {
+                        listaDatos.Add(new Tuple<string, string, double, double>(fecha, descripcion, importe_double, saldo_double));
+                    }
+                }
+            }
+        }
+
+        void CargarDataGridView()
+        {
+
+            foreach (Tuple<string, string, double, double> tuple in listaDatos)
+            {
+                dataGridView1.Rows.Add(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4);
+            }
+        }
+
+
+
+
+        void BuscarImporte(string contiene)
+        {
+            double sumatoriaMercado = 0.0;
+
+            foreach (Tuple<string, string, double, double> tuple in listaDatos)
+            {
+                if (tuple.Item2.Contains(contiene))
+                {
+                    sumatoriaMercado += tuple.Item3;
+                }
+            }
+
+            lbl_importe_buscado.Text = sumatoriaMercado.ToString();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Fecha,Descripción,Importe,Saldo
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        private void btn_sumatoria_Click(object sender, EventArgs e)
+        {
+            
+
+            listaDatos = new List<Tuple<string, string, double, double>>();
+            CargarListaDatos();
+            BuscarImporte(txt_contiene.Text);
+
+        }
+
+        private void btn_carga_datagrid_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.Rows.Clear(); 
+
+            listaDatos = new List<Tuple<string, string, double, double>>();
+            CargarListaDatos(txt_contiene.Text);
+            CargarDataGridView();
+
+        }
+
+        private void btn_reset_datagrid_Click(object sender, EventArgs e)
+        {
+            ResetDataGrid();
+        }
+    }
+}
